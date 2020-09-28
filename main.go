12ad05de672b903ba2d8b9c581be277cf2807854
Jayce S. Chant <jayce.chant@gmail.com>
2020-09-28 17:57:58 +0800

    feat: add version flag to to print current version info

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/JayceChant/commit-msg/validator"
+)
 
+const (
+	versionStr = "v0.1.2"
+)
+
+var (
+	versionFlag = flag.Bool("version", false, "")
 )
 
 func main() {
+	flag.Parse()
+	if *versionFlag {
+		printVersion(os.Args[0])
+		return
+	}
+
 	msgFile := ""
 	if len(os.Args) > 1 {
 		msgFile = os.Args[1]
 	}
 
 	validator.Validate(msgFile)
+}
+
+func printVersion(cmd string) {
+	fmt.Printf("%s %s\n", filepath.Base(cmd), versionStr)
 }
